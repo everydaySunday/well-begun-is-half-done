e38da06fd09b4c24907a98bd7c538b21e14d3a81
everydaySunday <48465277+everydaySunday@users.noreply.github.com>
2020-09-27 23:49:54 +0900

    Add files via upload

---
+++ studyolleh/src/test/java/com/studyolleh/account/AccountControllerTest.java
@@ -0,0 +1,34 @@
+package com.studyolleh.account;
+
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AccountControllerTest {
+	
+	@Autowired MockMvc mockMvc;
+	
+	@DisplayName("회원 가입 화면 보이는지 테스트")
+	@Test
+	void signUpForm() throws Exception {
+		mockMvc.perform(get("/sign-up"))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(view().name("account/sign-up"));
+			
+	}
+}
+
